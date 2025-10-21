8dfa6be4da30b27ace9fc01e3131109411fcc622
Ami0480 <amifuku80@gmail.com>
2025-10-21 15:44:43 +0800

    Installed fonts

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
+import icedCoffee from "./images/iced-coffee.png";
+
 function App() {
-  return <div className="text-5xl text-blue-400">Cafe</div>;
+  return (
+    <div>
+      <div
+        style={{ backgroundImage: `url(${icedCoffee})` }}
+        className="w-full h-screen flex items-center justify-center bg-cover bg-center"
+      >
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-quattro text-7xl text-white"> Cafe</h1>
+          <h2 className="font-alice text-2xl text-white ">in Perth</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        quattro: ["Quattrocento", "serif"],
+        alice: ["Alice", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
